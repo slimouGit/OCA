44dbe6c5c7848c6b7a78a634058e87c56b8199eb
MIMISOU\Salim <box@slimou.net>
2019-05-26 14:25:19 +0200

    added comment

--- _001_DeclarationAndAccessControl/_011_use_members_without_inheritance/Moo.java
+++ _001_DeclarationAndAccessControl/_011_use_members_without_inheritance/Moo.java
@@ -2,5 +2,6 @@ class Moo {
 	public void useAZoo(){
 		Zoo z = new Zoo();	
 		System.out.println("A Zoo says: " + z.coolMethod());
+		//the predicting line works, because Moo can access the public method
 	}
 }
