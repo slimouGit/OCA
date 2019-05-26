5e50d61a5767b4d557f152461982c234146daa99
MIMISOU\Salim <box@slimou.net>
2019-05-26 16:46:23 +0200

    inherit class and members

--- _001_DeclarationAndAccessControl/_012_use_members_with_inheritance/Moo.java
+++ _001_DeclarationAndAccessControl/_012_use_members_with_inheritance/Moo.java
@@ -3,9 +3,11 @@ class Moo extends Zoo {
 		System.out.println("Inherit Zoo says: " + this.coolMethod());
 		//the predicting line works, because Moo can inherit the public method
 		
+		
+
 		Zoo z = new Zoo();
 		System.out.println("A Zoo instance says: " + z.coolMethod());
 		//the predicting line works, because Moo can access the public method
-		
+				
 	}
 }
--- _001_DeclarationAndAccessControl/_012_use_members_with_inheritance/Zoo.java
+++ _001_DeclarationAndAccessControl/_012_use_members_with_inheritance/Zoo.java
@@ -1,6 +1,5 @@
 class Zoo {
 	public String coolMethod(){
 		return "Wow baby";
-	}
-
+	}	
 }
