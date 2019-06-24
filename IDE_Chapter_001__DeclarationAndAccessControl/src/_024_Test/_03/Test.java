ed821cd9d6b15102d8522ff890514882ab24e2e6
Salim Oussayfi <box@slimou.net>
2019-06-24 19:41:42 +0200

    strange args

---
+++ IDE_Chapter_001__DeclarationAndAccessControl/src/_024_Test/_03/Test.java
@@ -0,0 +1,19 @@
+package _024_Test._03;
+
+import static java.lang.System.out;
+
+class Test {
+    static public void main(String[] __A_V_) {
+        out.println("arg length " + __A_V_.length);
+        String $ = "";
+        for (int x = 0; x <= __A_V_.length;) {
+            $ += __A_V_[x];
+            out.println("arg length " + __A_V_.length);
+            out.println($);
+        }
+        /**
+         command line java _ - A .
+         Output is A.
+         */
+    }
+}
