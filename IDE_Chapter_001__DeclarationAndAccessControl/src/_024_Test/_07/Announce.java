4923e5824fa974debc429a5fb67b37bc4a1b98d4
Salim Oussayfi <box@slimou.net>
2019-06-26 19:33:36 +0200

    test 07

---
+++ IDE_Chapter_001__DeclarationAndAccessControl/src/_024_Test/_07/Announce.java
@@ -0,0 +1,14 @@
+package _024_Test._07;
+
+
+class Announce {
+
+    public static void main(String[] args){
+        for (int __x = 0; __x < 3; __x++){
+            // int #1b = 7;
+            // long [] x [5];
+            Boolean []ba[];
+        }
+    }
+
+}
