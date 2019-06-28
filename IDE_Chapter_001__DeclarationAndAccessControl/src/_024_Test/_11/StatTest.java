469fa3228bba243d6b9ea56f03c10342910179b3
Salim Oussayfi <box@slimou.net>
2019-06-28 18:48:40 +0200

    import from java.lang.*

---
+++ IDE_Chapter_001__DeclarationAndAccessControl/src/_024_Test/_11/StatTest.java
@@ -0,0 +1,12 @@
+package _024_Test._11;
+
+/*
+import static java.lang.Integer.*
+import static java.lang.Integer.MAX_VALUE;
+ */
+
+public class StatTest {
+    public static void main(String[] args){
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
