2512b092b11843ccd5691988c67edc880faea27b
Salim Oussayfi <box@slimou.net>
2019-07-06 16:19:17 +0200

    access to subclasses

---
+++ IDE_Chapter_001__DeclarationAndAccessControl/src/_000_Sandbox/inheritance_03/Frodo.java
@@ -0,0 +1,28 @@
+package _000_Sandbox.inheritance_03;
+
+
+public class Frodo extends Hobbit{
+    public static void main(String[] args) {
+        int myGold = 40;
+        /**
+         * NOT POSSIBLE CANNOT BE INVOKE FROM STATIC CONTEXT
+
+         System.out.println(countGold(myGold, 2));
+
+
+         */
+
+
+
+        Hobbit hobbit = new Hobbit();
+        System.out.println("Hobbit: " + hobbit.countGold(myGold, hobbit.hobbitGold));
+
+
+
+        System.out.println("Frodo: " + new Frodo().hobbitGold());
+    }
+
+    int hobbitGold(){
+        return countGold(40, hobbitGold);
+    }
+}
---
+++ IDE_Chapter_001__DeclarationAndAccessControl/src/_000_Sandbox/inheritance_03/Hobbit.java
@@ -0,0 +1,8 @@
+package _000_Sandbox.inheritance_03;
+
+public class Hobbit {
+    public int hobbitGold = 2;
+    int countGold(int x, int y){
+        return x+y;
+    }
+}
