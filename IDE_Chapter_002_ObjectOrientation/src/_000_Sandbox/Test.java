7d22e5e49dd73a64b1e155b912e672d7b9cf7ceb
Salim Oussayfi <box@slimou.net>
2019-07-06 19:14:02 +0200

    getter/setter

--- IDE_Chapter_002_ObjectOrientation/src/_000_Sandbox/Test.java
+++ IDE_Chapter_002_ObjectOrientation/src/_000_Sandbox/Test.java
@@ -1,9 +1,5 @@
-package _001_Sandbox;
+package _000_Sandbox;
 
-/**
- * User: salim
- * Date: 06.07.2019 19:08
- */
 public class Test {
     public static void main(String[] args){
         System.out.println("Test");
---
+++ IDE_Chapter_002_ObjectOrientation/src/_001_GetrerSetter/Box.java
@@ -0,0 +1,13 @@
+package _001_GetrerSetter;
+
+public class Box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
