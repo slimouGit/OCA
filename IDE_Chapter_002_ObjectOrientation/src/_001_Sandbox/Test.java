7179d846faba49b744f6ef90d02e8a2f12f2cb47
Salim Oussayfi <box@slimou.net>
2019-07-06 19:10:15 +0200

    chapter 2

---
+++ IDE_Chapter_002_ObjectOrientation/src/_001_Sandbox/Test.java
@@ -0,0 +1,11 @@
+package _001_Sandbox;
+
+/**
+ * User: salim
+ * Date: 06.07.2019 19:08
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Test");
+    }
+}
