cf06d8e3ac4bd11b56b9c28ba1e37df37dec4155
Salim Oussayfi <box@slimou.net>
2019-07-14 11:52:02 +0200

    commented

--- IDE_Chapter_002_ObjectOrientation/src/_005_Polymorphism_01/PlayerPiece.java
+++ IDE_Chapter_002_ObjectOrientation/src/_005_Polymorphism_01/PlayerPiece.java
@@ -1,5 +1,12 @@
 package _005_Polymorphism_01;
 
+/**
+ * PlayerPiece IS-A:
+ * An Object (since any object inherits from Object)
+ * A GameShape (since PlayerPiece extends GameShape)
+ * A PlayerPiece (since that’s what it really is)
+ * An Animatable (since PlayerPiece implements Animatable)
+ */
 public class PlayerPiece extends GameShape implements Animatable {
     public void animate() {
         System.out.println("animating ...");
