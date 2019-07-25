7fa8b59bc2ca55511807e4b3d32cda9d0eb7a466
Salim Oussayfi <box@slimou.net>
2019-07-25 15:37:51 +0200

    constructor fails

---
+++ IDE_Chapter_002_ObjectOrientation/src/_016_ConstructorFAIL/ConstructorFail.java
@@ -0,0 +1,22 @@
+package _016_ConstructorFAIL;
+
+public class ConstructorFail {
+
+    //RECURSIV INVOKING
+    public ConstructorFail() {
+        this("foo");
+    }
+    public ConstructorFail(String s) {
+        this();
+    }
+
+    //ITS LIKE THE FOLLOWING CODE
+    //MACHINE WILL EXPLODE
+    public void go(){
+        doStuff();
+    }
+
+    private void doStuff() {
+        go();
+    }
+}
