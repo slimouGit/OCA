f6af5a63b884dc8d5dd13e5c65f5e3b43127fdc9
Salim Oussayfi <box@slimou.net>
2019-07-25 15:03:32 +0200

    simple constructor

--- IDE_Chapter_002_ObjectOrientation/src/_014_Contructor/UseBar.java
+++ IDE_Chapter_002_ObjectOrientation/src/_014_Contructor/UseBar.java
@@ -4,6 +4,12 @@ public class UseBar extends Bar {
     public UseBar(int x, int y, String z) {
         super(x, y, z);
     }
+
+    /**
+     NOT ALLOWED, BECAUSE NO DEFAULT CONSTRUCTOR IN SUPERCLASS
+    public UseBar(){}
+     */
+
     public static void main(String[] args){
 
     }
