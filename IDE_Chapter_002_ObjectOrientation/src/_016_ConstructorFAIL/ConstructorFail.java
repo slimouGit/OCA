befb8f350d1160e6f474063c7363df61cb25d0c5
Salim Oussayfi <box@slimou.net>
2019-07-27 10:29:39 +0200

    comment out failed code

--- IDE_Chapter_002_ObjectOrientation/src/_016_ConstructorFAIL/ConstructorFail.java
+++ IDE_Chapter_002_ObjectOrientation/src/_016_ConstructorFAIL/ConstructorFail.java
@@ -2,13 +2,18 @@ package _016_ConstructorFAIL;
 
 public class ConstructorFail {
 
-    //RECURSIV INVOKING
-    public ConstructorFail() {
+    /**
+
+     RECURSIV INVOKING
+
+     public ConstructorFail() {
         this("foo");
     }
     public ConstructorFail(String s) {
         this();
     }
+     
+     **/
 
     //ITS LIKE THE FOLLOWING CODE
     //MACHINE WILL EXPLODE
