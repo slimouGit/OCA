9d954ef92429466b06842794bcb7ab92c34da632
Salim Oussayfi <box@slimou.net>
2019-08-24 16:30:27 +0200

    no no-arg constructor inheritance

---
+++ IDE_Chapter_002_ObjectOrientation/src/_024_Test/_02/Bottom2.java
@@ -0,0 +1,12 @@
+package _024_Test._02;
+
+/**
+ * User: salim
+ * Date: 24.08.2019 16:26
+ */
+public class Bottom2 extends Top {
+    public Bottom2(String s) {
+        super(s); //Must be, because ther is no no arg constructor in Top
+        System.out.println("D");
+    }
+}
---
+++ IDE_Chapter_002_ObjectOrientation/src/_024_Test/_02/Top.java
@@ -0,0 +1,12 @@
+package _024_Test._02;
+
+/**
+ * User: salim
+ * Date: 24.08.2019 16:25
+ */
+public class Top {
+    //NO DEFAULT CONSTRUCTOR!!!
+    public Top(String s){
+        System.out.println("B");
+    }
+}
