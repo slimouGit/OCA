17690531b9d78fedadac1d7cd92cbd611cdb9852
Salim Oussayfi <box@slimou.net>
2019-09-08 11:36:12 +0200

    inhertitance and constructors behaviour

--- IDE_Chapter_002_ObjectOrientation/src/_000_Sandbox/_012_Inheritance_ConstuctorsBehaviour/FamiliyTest.java
+++ IDE_Chapter_002_ObjectOrientation/src/_000_Sandbox/_012_Inheritance_ConstuctorsBehaviour/FamiliyTest.java
@@ -2,8 +2,8 @@ package _000_Sandbox._012_Inheritance_ConstuctorsBehaviour;
 
 public class FamiliyTest {
     public static void main(String[] args){
-        new Son_1(12);
-        new Son_2(10);
+        new Son_1(12); //extends mother without no arg constructor
+        new Son_2(10); //extends vather with a no ar constructor too
     }
 
     /*
