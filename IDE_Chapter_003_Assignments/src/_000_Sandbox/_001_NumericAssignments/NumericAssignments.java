f36e6707b0cd53d4676cf6cfc4f5b2fb385ee793
Salim Oussayfi <box@slimou.net>
2019-09-18 19:15:37 +0200

    numeric assignments

---
+++ IDE_Chapter_003_Assignments/src/_000_Sandbox/_001_NumericAssignments/NumericAssignments.java
@@ -0,0 +1,17 @@
+package _000_Sandbox._001_NumericAssignments;
+
+public class NumericAssignments {
+    public static void main(String[] args){
+        int x = 0x0001;
+        System.out.println("0x0001 \t\t" + x);
+
+        int y = 0x7fffffff;
+        System.out.println("0x7fffffff \t" + y);
+
+        int z = 0xDeadCafe;
+        System.out.println("0xDeadCafe \t" + z);
+
+        int w = 0xCafe;
+        System.out.println("0xCafe \t\t" + w);
+    }
+}
