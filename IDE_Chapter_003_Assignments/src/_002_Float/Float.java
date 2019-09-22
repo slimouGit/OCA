0f16c7feef259d3ae5da0c522ca7da193c3a134f
Salim Oussayfi <box@slimou.net>
2019-09-22 11:30:12 +0200

    Float and double

---
+++ IDE_Chapter_003_Assignments/src/_002_Float/Float.java
@@ -0,0 +1,13 @@
+package _002_Float;
+
+public class Float {
+    public static void main(String[] args){
+        //float a = 42.222; // not possible
+        float b = 42.22f;
+        float c = 42.22F;
+        float d = (float)42.22;
+
+        //Double values need not to be casted
+        double e = 42.22;
+    }
+}
