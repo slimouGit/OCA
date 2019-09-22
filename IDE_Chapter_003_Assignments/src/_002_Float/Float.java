56b92728421eb55df159231fe8d14df7f05f6701
MIMISOU\Salim <box@slimou.net>
2019-09-22 11:43:18 +0200

    added comment

--- IDE_Chapter_003_Assignments/src/_002_Float/Float.java
+++ IDE_Chapter_003_Assignments/src/_002_Float/Float.java
@@ -3,6 +3,7 @@ package _002_Float;
 public class Float {
     public static void main(String[] args){
         //float a = 42.222; // not possible
+        //float values has to be declared as float values
         float b = 42.22f;
         float c = 42.22F;
         float d = (float)42.22;
