3a5dbf98b6931d4d16f3377f0df9f98624bdf74b
Salim Oussayfi <box@slimou.net>
2019-10-04 15:44:00 +0200

    boolean operator in useless way

---
+++ IDE_Chapter_004_Operators/src/_000_Sandbox/_001_Boolean_Operator/BooleanOperator.java
@@ -0,0 +1,8 @@
+package _000_Sandbox._001_Boolean_Operator;
+
+public class BooleanOperator {
+    public static void main(String[] args){
+        boolean b = 99 > 100;
+        System.out.println(b);
+    }
+}
