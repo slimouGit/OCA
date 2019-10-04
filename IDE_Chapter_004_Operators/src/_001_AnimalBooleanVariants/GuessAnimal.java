08e1fd70f4cc0b9282afbdd02a77eb7836f4e2b4
Salim Oussayfi <box@slimou.net>
2019-10-04 15:52:04 +0200

    boolean in praxis

---
+++ IDE_Chapter_004_Operators/src/_001_AnimalBooleanVariants/GuessAnimal.java
@@ -0,0 +1,17 @@
+package _001_AnimalBooleanVariants;
+
+public class GuessAnimal {
+    public static void main(String[] args) {
+        String animal = "unknown";
+        int weight = 700;
+        char sex = 'm';
+        double colorWaveLength = 1.630;
+
+        if (weight >= 500) animal = "elephant";
+        if (colorWaveLength > 1.621) animal = "gray " + animal;
+        if (sex <= 'f') animal = "female " + animal;
+
+        System.out.println("The animal is a " + animal);
+    }
+
+}
