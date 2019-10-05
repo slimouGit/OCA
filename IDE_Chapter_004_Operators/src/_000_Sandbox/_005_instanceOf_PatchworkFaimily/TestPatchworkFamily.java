043b16af600f6bb148e2a29b44a6ae56bc69f0c6
MIMISOU\Salim <box@slimou.net>
2019-10-05 17:37:11 +0200

    instance of

---
+++ IDE_Chapter_004_Operators/src/_000_Sandbox/_005_instanceOf_PatchworkFaimily/TestPatchworkFamily.java
@@ -0,0 +1,19 @@
+package _000_Sandbox._005_instanceOf_PatchworkFaimily;
+
+public class TestPatchworkFamily {
+    public static void main(String[] args){
+        Grandpa grandpa = new Grandpa();
+        Vather vather = new Vather();
+        Son son = new Son();
+        AdoptedDaughter adoptedDaughter = new AdoptedDaughter();
+
+        System.out.println(grandpa instanceof fightWar); //TRUE
+        System.out.println(grandpa instanceof Vather); //FALSE
+        System.out.println(grandpa instanceof work); //FALSE
+        System.out.println(vather instanceof fightWar); //TRUE
+        System.out.println(vather instanceof Grandpa); //TRUE
+        System.out.println(son instanceof Grandpa); //TRUE
+        //System.out.println(adoptedDaughter instanceof Vather); //INCOMPATIBLE TYPE!!!
+        System.out.println(adoptedDaughter instanceof work); //FALSE
+    }
+}
---
+++ IDE_Chapter_004_Operators/src/_000_Sandbox/_005_instanceOf_PatchworkFaimily/Vather.java
@@ -0,0 +1,4 @@
+package _000_Sandbox._005_instanceOf_PatchworkFaimily;
+
+public class Vather extends Grandpa implements work {
+}
