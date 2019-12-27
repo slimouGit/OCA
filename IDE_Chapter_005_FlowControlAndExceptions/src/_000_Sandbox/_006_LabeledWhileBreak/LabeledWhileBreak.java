8af52b6350a77fed343c77925d5ff94488bbdbc9
Salim Oussayfi <box@slimou.net>
2019-12-27 17:03:53 +0100

    labeled loop condition

---
+++ IDE_Chapter_005_FlowControlAndExceptions/src/_000_Sandbox/_006_LabeledWhileBreak/LabeledWhileBreak.java
@@ -0,0 +1,18 @@
+package _000_Sandbox._006_LabeledWhileBreak;
+
+public class LabeledWhileBreak {
+    public static void main(String[] args) {
+        int age = 0;
+        int ageOfDeath = 110;
+        growUp:
+        while (age < ageOfDeath) {
+            age++;
+            System.out.println("current age " + age);
+            if (age == 18) {
+                System.out.println("got driverlicense in age of " + age);
+                break growUp;
+            }
+            continue growUp;
+        }
+    }
+}
