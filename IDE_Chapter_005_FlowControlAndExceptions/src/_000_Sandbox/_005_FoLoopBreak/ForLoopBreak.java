f3ef55a70f57a8c28cfc38b65cb15290cba8cda7
Salim Oussayfi <box@slimou.net>
2019-12-27 16:54:30 +0100

    labeled break

--- IDE_Chapter_005_FlowControlAndExceptions/src/_000_Sandbox/_005_FoLoopBreak/ForLoopBreak.java
+++ IDE_Chapter_005_FlowControlAndExceptions/src/_000_Sandbox/_005_FoLoopBreak/ForLoopBreak.java
@@ -1,14 +1,16 @@
 package _000_Sandbox._005_FoLoopBreak;
 
 public class ForLoopBreak {
-    public static void main(String[] args){
-        for(int i = 0; i<10;i++){
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Inside loop " + i);
-            if(i==5){
+            if (i == 5) {
                 break;
             }
 
         }
-            System.out.println("outside loop ");
+        System.out.println("outside loop ");
+
+
     }
 }
---
+++ IDE_Chapter_005_FlowControlAndExceptions/src/_004_LabeledBreak/LabeledBreak.java
@@ -0,0 +1,17 @@
+package _004_LabeledBreak;
+
+
+public class LabeledBreak {
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        outer:
+        for (int i = 0; i < 5; i++) {
+            while (isTrue) {
+                System.out.println("Hello");
+                break outer;
+            }
+            System.out.println("Outer loop");
+        }
+            System.out.println("Bye bye");
+    }
+}
