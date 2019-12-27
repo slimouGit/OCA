1c5fea0a2c699ee58d515c5ea2207b5dc3224559
Salim Oussayfi <box@slimou.net>
2019-12-27 10:43:45 +0100

    do loop

---
+++ IDE_Chapter_005_FlowControlAndExceptions/src/_000_Sandbox/_004_DoLoop/DoLoop.java
@@ -0,0 +1,12 @@
+package _000_Sandbox._004_DoLoop;
+
+public class DoLoop {
+    public static void main(String[] args) {
+        int x = 10;
+        do {
+            System.out.println("x is: " + x);
+            --x;
+        }
+        while (x > 2);
+    }
+}
