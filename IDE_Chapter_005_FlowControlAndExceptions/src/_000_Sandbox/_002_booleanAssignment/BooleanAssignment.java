06c17ed2e5600ea11806642515e45a0196bc3a50
Salim Oussayfi <box@slimou.net>
2019-12-27 10:04:59 +0100

    boolean assignment

---
+++ IDE_Chapter_005_FlowControlAndExceptions/src/_000_Sandbox/_002_booleanAssignment/BooleanAssignment.java
@@ -0,0 +1,14 @@
+package _000_Sandbox._002_booleanAssignment;
+
+public class BooleanAssignment {
+    public static void main(String[] args){
+        boolean boo = false;
+        if(boo = true){
+            System.out.println(boo);
+        }
+
+        /**
+         * OUT: true
+         */
+    }
+}
