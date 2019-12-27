79b568a640ef1fc1dffd87cc336e634b304cb82e
Salim Oussayfi <box@slimou.net>
2019-12-27 10:38:33 +0100

    while loop

---
+++ IDE_Chapter_005_FlowControlAndExceptions/src/_000_Sandbox/_003_WhileLoop/WhileLoop.java
@@ -0,0 +1,11 @@
+package _000_Sandbox._003_WhileLoop;
+
+public class WhileLoop {
+    public static void main(String[] args){
+        int x = 10;
+        while (x>2){
+            System.out.println("x is: "+ x);
+            --x;
+        }
+    }
+}
