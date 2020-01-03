b4d56ee9c557a70b4fa97bba04d58954b77d1a76
Salim Oussayfi <box@slimou.net>
2020-01-03 16:22:25 +0100

    String test

---
+++ IDE_Chapter_006_StringsArraysArrayListsDatesLambdas/src/_001_Sandbox/_001_StringObjectConcat/ConcatString.java
@@ -0,0 +1,16 @@
+package _001_Sandbox._001_StringObjectConcat;
+
+/**
+ * User: salim
+ * Date: 03.01.2020 16:19
+ */
+public class ConcatString {
+    public static void main(String[] args){
+        String s = "Java";
+        System.out.println(s); //Java
+        s.concat(" Geek");
+        System.out.println(s); //still Java
+        s = s.concat(" Geek");
+        System.out.println(s); //now Java Geek
+    }
+}
