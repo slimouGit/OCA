57fa374d04599f4c1fcc3c1a9e9c65ca0f56a7f4
MIMISOU\Salim <box@slimou.net>
2020-01-04 14:55:06 +0100

    StringBuilder insert Method

---
+++ IDE_Chapter_006_StringsArraysArrayListsDatesLambdas/src/_004_StringBuilder_insert/StringBuilder_insert.java
@@ -0,0 +1,8 @@
+package _004_StringBuilder_insert;
+
+public class StringBuilder_insert {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("0123456789");
+        System.out.println(sb.insert(4,"---")); //0123---456789 
+    }
+}
