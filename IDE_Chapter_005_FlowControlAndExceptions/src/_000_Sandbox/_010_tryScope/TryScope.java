4ca46406296e2fc7fb72d0357dfc2d44866beccd
Salim Oussayfi <box@slimou.net>
2020-03-23 11:33:30 +0100

    try block scope

---
+++ IDE_Chapter_005_FlowControlAndExceptions/src/_000_Sandbox/_010_tryScope/TryScope.java
@@ -0,0 +1,20 @@
+package _000_Sandbox._010_tryScope;
+
+public class TryScope {
+    static int thrower() throws Exception{
+        return 42;
+    }
+    public static void main(String[] args){
+        try{
+            int x = thrower();
+        } catch (Exception e) {
+           // x++;
+        }finally {
+           // System.out.print("x = "+ ++x);
+        }
+    }
+
+    /**
+     * x is only in the scope within the try block
+     */
+}
