6871ac5d0cde42ae1d6052961e983aef6f97d845
Salim Oussayfi <box@slimou.net>
2020-03-28 13:48:21 +0100

    literaly

---
+++ TestPreparation/src/_005_Literally/Literally.java
@@ -0,0 +1,21 @@
+package _005_Literally;
+
+public class Literally {
+    public static void main(String[] args){
+        int i1 = 1_000;
+        int i2 = 10_00;
+        int i3 = 0b101010;
+        int i4 = 0B10_1010;
+        int i5 = 0x2_a;
+        /**
+         //underscore cant include at the beginning of numeric literals
+         int i6 = _10_000
+         */
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+        System.out.println(i5);
+    }
+}
