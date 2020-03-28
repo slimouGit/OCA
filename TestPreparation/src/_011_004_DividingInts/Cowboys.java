896e29b644612f16cdbdc8c0d211634b39f9a497
Salim Oussayfi <box@slimou.net>
2020-03-28 17:41:24 +0100

    dividing ints

---
+++ TestPreparation/src/_011_004_DividingInts/Cowboys.java
@@ -0,0 +1,12 @@
+package _011_004_DividingInts;
+
+public class Cowboys {
+    public static void main(String[] args) {
+        int x = 12;
+        int a = 5;
+        int b = 7;
+        System.out.println(x/a + " " + x/b);
+        //When dividing ints, remainders are always rounded down
+    }
+
+}
