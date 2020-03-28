8eeb6b8c461512ea475285e30dfd25ca1642852f
Salim Oussayfi <box@slimou.net>
2020-03-28 17:49:56 +0100

    compare enums in loop with break condition

---
+++ TestPreparation/src/_012_004_ComparingEnums/McGee.java
@@ -0,0 +1,19 @@
+package _012_004_ComparingEnums;
+
+public class McGee {
+    public static void main(String[] args) {
+        Days d1 = Days.TH;
+        Days d2 = Days.M;
+
+        for(Days d:Days.values()){
+            if(d.equals(Days.F)) break;
+            d2 = d;
+        }
+        System.out.println((d1 == d2)?"same old":"newly new");
+
+    }
+
+    enum Days {M, T, W, TH, F, SA, SU}
+
+    ;
+}
