eaf74d5946911d4a17b601fbb7b3d1f2b22a667c
Salim Oussayfi <box@slimou.net>
2020-03-29 14:31:25 +0200

    multidimensional array casting

---
+++ TestPreparation/src/_024_MultiDimensionalArrayCasting/Dims.java
@@ -0,0 +1,11 @@
+package _024_MultiDimensionalArrayCasting;
+
+public class Dims {
+    public static void main(String[] args){
+        int[][] a = {{1,2},{3,4}};
+        System.out.println(a[1].length);
+        int[] b = (int[]) a[1];
+        for(int x:b)  System.out.println(x);
+        System.out.println(b);
+    }
+}
