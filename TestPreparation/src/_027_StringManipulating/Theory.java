2ca62508f53bd0b16fbe93e4f2b340fb84256b36
Salim Oussayfi <box@slimou.net>
2020-03-29 14:49:02 +0200

    manipulate and compare Strings and StringBuilder

---
+++ TestPreparation/src/_027_StringManipulating/Theory.java
@@ -0,0 +1,17 @@
+package _027_StringManipulating;
+
+public class Theory {
+    public static void main(String[] args){
+        String s1 = "abc";
+        String s2 = s1;
+        System.out.println(s1 + " " + s2 + " " + (s1==s2));     //abc abc true
+        s1 += "d";
+        System.out.println(s1 + " " + s2 + " " + (s1==s2));     //abcd abc false
+
+        StringBuilder sb1 = new StringBuilder("abc");
+        StringBuilder sb2 = sb1;
+        sb2.append("d");
+        System.out.println(sb1 + " " + sb2 + " " + (sb1==sb2)); //abcd abcd true
+
+    }
+}
