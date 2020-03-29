205aa29c4720d8732cd82981db55c3352689671d
Salim Oussayfi <box@slimou.net>
2020-03-29 09:41:07 +0200

    ExceptionHandling comitted

--- TestPreparation/src/_017_005_ExceptionHandling/Plane.java
+++ TestPreparation/src/_017_005_ExceptionHandling/Plane.java
@@ -3,26 +3,26 @@ package _017_005_ExceptionHandling;
 public class Plane {
     static String s = "-";
     public static void main(String[] args){
-        new Plane().s1();
-        System.out.println(s);
+        new Plane().s1();                   //1.
+        System.out.println(s);              //6.        -c
     }
 
     private void s1() {
         try {
-            s2();
-        }catch (Exception e){
-            s += "c";
+            s2();                           //2.
+        }catch (Exception e){               //Exception is catched here
+            s += "c";                       //5.
         }
     }
 
-    private void s2() throws Exception {
-        s3();
+    private void s2() throws Exception {    //Exception is thrown to s1
+        s3();                               //3.
         s+= "2";
         s3();
         s+= "2b";
     }
 
-    private void s3() throws Exception {
-        throw  new Exception();
+    private void s3() throws Exception {    //Exception is thrown to s2
+        throw  new Exception();             //4.
     }
 }
