43273814911dda2d8cf792520a5f27fcca294ae1
Salim Oussayfi <box@slimou.net>
2020-03-29 17:09:39 +0200

    String manipulating

---
+++ TestPreparation/src/_030_StringManipulation/Hedges.java
@@ -0,0 +1,14 @@
+package _030_StringManipulation;
+
+public class Hedges {
+    public static void main(String[] args){
+        String s = "JAVA";
+        System.out.println(s);  //JAVA
+        s = s + "rocks";
+        System.out.println(s);  //JAVArocks
+        s = s.substring(4,8);
+        System.out.println(s);  //rocks
+        s.toUpperCase();        // new String Object
+        System.out.println(s);  //rocks
+    }
+}
