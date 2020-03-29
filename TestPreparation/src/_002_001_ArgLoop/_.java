9b9837d3dea4f03fc2adedf5b0f9bea8e239915b
Salim Oussayfi <box@slimou.net>
2020-03-29 18:04:34 +0200

    arg loop

---
+++ TestPreparation/src/_002_001_ArgLoop/_.java
@@ -0,0 +1,12 @@
+package _002_001_ArgLoop;
+import static java.lang.System.*;
+
+public class _ {
+    static public void main(String[] __A_V){
+        String $ = "";
+        for(int x = 0; ++x <4;x++){
+            $ += x;
+        }
+        out.println($); //1 3 
+    }
+}
