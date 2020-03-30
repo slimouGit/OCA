097e5e27b644b666bc4e812f2e6c92edd81e207f
Salim Oussayfi <box@slimou.net>
2020-03-30 17:32:25 +0200

    Evil syntaxt

---
+++ TestPreparation/src/_001_005_EvilSyntax/Announce.java
@@ -0,0 +1,11 @@
+package _001_005_EvilSyntax;
+
+public class Announce {
+    public static void main(String[] args){
+        for (int __x = 0; __x < 3; __x++){
+            //int #1b = 7;  EVIL cause #
+            //long[]x[5];   EVIL cause [5]
+            Boolean[]ba[];
+        }
+    }
+}
