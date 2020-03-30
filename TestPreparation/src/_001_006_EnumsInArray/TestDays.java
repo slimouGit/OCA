0e8243ff9823155b93126ca202d8ceb3eca83e9b
Salim Oussayfi <box@slimou.net>
2020-03-30 17:36:32 +0200

    initialize an enum array

---
+++ TestPreparation/src/_001_006_EnumsInArray/TestDays.java
@@ -0,0 +1,12 @@
+package _001_006_EnumsInArray;
+
+public class TestDays {
+    public enum Days {MON,TUE,WED};
+    public static void main(String[] args){
+        for(Days d : Days.values())
+            ;
+        Days [] d2 = Days.values();
+        System.out.println(d2[2]);
+
+    }
+}
