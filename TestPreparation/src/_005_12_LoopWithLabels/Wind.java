002b67bbabf5902a2a48eebf713cefb7bef78a1e
Salim Oussayfi <box@slimou.net>
2020-04-30 17:37:17 +0200

    labeled loop

---
+++ TestPreparation/src/_005_12_LoopWithLabels/Wind.java
@@ -0,0 +1,14 @@
+package _005_12_LoopWithLabels;
+
+public class Wind {
+    public static void main(String[] args){
+        foreach:
+        for(int j =0;j<5;j++){
+            for(int k=0;k<3;k++){
+                System.out.print(" " + j);      //0 1 1 1 2 3 3
+                if(j==3 && k==1) break foreach;
+                if(j==0 || j==2) break;
+            }
+        }
+    }
+}
