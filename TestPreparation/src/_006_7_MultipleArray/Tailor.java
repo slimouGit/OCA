2e5d4c7eb56a8ce4741efe84ab880bfbfbfd6bd9
Salim Oussayfi <box@slimou.net>
2020-05-01 10:51:09 +0200

    Multidimensional Array

---
+++ TestPreparation/src/_006_7_MultipleArray/Tailor.java
@@ -0,0 +1,8 @@
+package _006_7_MultipleArray;
+
+public class Tailor {
+    public static void main(String[] args){
+        byte[][] ba = {{1,2,3,4},{1,2,3}};
+        System.out.println(ba[0].length + " " + ba[1].length + " " + ba.length); //4 3 2
+    }
+}
