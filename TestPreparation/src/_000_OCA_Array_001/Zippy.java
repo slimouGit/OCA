37b2b74f082da9baab0475e448e3f077fc14664d
Salim Oussayfi <box@slimou.net>
2020-09-20 11:41:28 +0200

    print arrays

--- TestPreparation/src/_000_OCA_Array_001/Zippy.java
+++ TestPreparation/src/_000_OCA_Array_001/Zippy.java
@@ -6,5 +6,14 @@ public class Zippy {
         int[] a [] = {{1,2},{1}};
         Object c = new long[4];
         Object[] d = x;
+
+        System.out.println("x = " + x[0]);
+        System.out.println("a 0 0 = " + a[0][0]);
+        System.out.println("a 0 1 = " + a[0][1]);
+        System.out.println("a 1 0 = " + a[1][0]);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d[0]);
+
+
     }
 }
