3cdd47b975c744536671055bad092e1cf1077b70
Salim Oussayfi <box@slimou.net>
2020-09-24 16:50:02 +0200

    wrong import statement

---
+++ TestPreparation/src/_001_010_Imports/WrongImports.java
@@ -0,0 +1,14 @@
+package _001_010_Imports;
+
+//WRON IMPORT
+import java.lang.*;
+
+//RIGHT IMPORT
+import java.util.ArrayList;
+import java.util.List;
+
+public class WrongImports {
+    public static void main(String[] args){
+        List<String> myPets = new ArrayList<>();
+    }
+}
